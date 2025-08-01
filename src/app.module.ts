c9ff64168175bff287013d212e2ef0d0d18dba25
Seereen Yangpaknam <seereen_2540@gmail.com>
2025-08-01 15:17:58 +0700

    refactor: Consolidate ConfigModule imports in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { getDatabaseConfig } from './config/database.config';
 import { AuthModule } from './auth/auth.module';
 import { EmployeesModule } from './employees/employees.module';
