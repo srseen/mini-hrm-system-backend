b602bf9b0ac3d29415f9b7b2387dde557d9d7a0c
Seereen Yangpaknam <seereen.2540@gmail.com>
2025-08-01 14:55:21 +0700

    Fix HRM System Errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-=======
 import { ConfigModule } from '@nestjs/config';
 import { getDatabaseConfig } from './config/database.config';
 import { AuthModule } from './auth/auth.module';
 import { EmployeesModule } from './employees/employees.module';
 import { DepartmentsModule } from './departments/departments.module';
 import { PositionsModule } from './positions/positions.module';
->>>>>>> 77e3e419ebc681ce765a16a36f9174932ac24d75
 
 @Module({
   imports: [
@@ -23,23 +16,6 @@ import { PositionsModule } from './positions/positions.module';
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
-<<<<<<< HEAD
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: parseInt(config.get<string>('DB_PORT') || '5432', 10),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true, // ตั้งค่า true ชั่วคราวตอน dev
-      }),
-    }),
-    AuthModule,
-    UserModule,
-=======
       useFactory: getDatabaseConfig,
       inject: [ConfigService],
     }),
@@ -47,7 +23,6 @@ import { PositionsModule } from './positions/positions.module';
     EmployeesModule,
     DepartmentsModule,
     PositionsModule,
->>>>>>> 77e3e419ebc681ce765a16a36f9174932ac24d75
   ],
   controllers: [AppController],
   providers: [AppService],
